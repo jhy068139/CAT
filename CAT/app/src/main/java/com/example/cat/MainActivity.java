4e69b617eabf0f7b6f956e8c5127cddccab4b90e
jhy068139 <64052098+jhy068139@users.noreply.github.com>
2020-06-30 20:46:06 +0900

    main activity xml 파일 마무리 3

--- CAT/app/src/main/java/com/example/cat/MainActivity.java
+++ CAT/app/src/main/java/com/example/cat/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.cat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calMenu;
+    TextView sel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        calMenu = (CalendarView)findViewById(R.id.calMenu);
+        sel = (TextView)findViewById(R.id.sel);
+
+        calMenu.setOnDateChangeListener(new CalendarView.OnDateChangeListener() // 날짜 선택 이벤트
+        {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth)
+            {
+                String date = year + "/" + (month + 1) + "/" + dayOfMonth;
+                sel.setText(date); // 선택한 날짜로 설정
+
+            }
+        });
     }
 }
