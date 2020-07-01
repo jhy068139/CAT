f9d7b3dc0cd862f0e619e2cfd73809ed159defd9
jhy068139 <64052098+jhy068139@users.noreply.github.com>
2020-07-01 17:49:34 +0900

    메뉴 만들기

--- CAT/app/src/main/java/com/example/cat/MainActivity.java
+++ CAT/app/src/main/java/com/example/cat/MainActivity.java
@@ -1,45 +1,26 @@
 package com.example.cat;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.lifecycle.MutableLiveData;
-
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import android.os.Bundle;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView itemMenu;
     CalendarView calMenu;
     TextView sel,tag,selname;
-
 
 
     @Override
@@ -50,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
         sel = findViewById(R.id.sel);
         tag = findViewById(R.id.tag);
         selname = findViewById(R.id.selname);
+        itemMenu = findViewById(R.id.itemMenu);
+
+        itemMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu p = new PopupMenu(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        v); // anchor : 팝업을 띄울 기준될 위젯
+                getMenuInflater().inflate(R.menu.item, p.getMenu());
+                // 이벤트 처리
+                p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getApplicationContext(),
+                                "팝업메뉴 이벤트 처리 - "
+                                        + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+
+
+                p.show(); // 메뉴를 띄우기
+
+            }
+        });
 
 
         calMenu = findViewById(R.id.calMenu);
@@ -64,5 +71,39 @@ public class MainActivity extends AppCompatActivity {
                 customDialog.callFunction(selname,sel,tag);
           }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.item, menu);
+
+        return true;
+    }
+
+    public void dd(View v) {
+        Toast.makeText(getApplicationContext(), "dd", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        Toast toast = Toast.makeText(getApplicationContext(),"", Toast.LENGTH_LONG);
+
+        switch(item.getItemId())
+        {
+            case R.id.menu1:
+                toast.setText("Select Menu1");
+                break;
+            case R.id.menu2:
+                toast.setText("Select Menu2");
+                break;
+        }
+
+        toast.show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
